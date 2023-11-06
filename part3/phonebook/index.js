16f91d445d7e8011c312e4df635f8670a1b5f466
Sushant Bajracharya <bajra_sushant@gmail.com>
2023-11-06 09:27:51 +0545

    fixes

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,21 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 
+const reqLogger = morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+      JSON.stringify(req.body),
+    ].join(' ')
+  })
+
 app.use(express.json())
+app.use(reqLogger)
 
 let data = [
     { 
@@ -95,3 +109,4 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
   }
 )
+
